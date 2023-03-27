83bcadd2ca9f4101c6c6cab98ac514aa90790c86
ronal <ronal@172.22.50.28>
2023-03-27 08:57:23 +0200

    Creata classe studente

---
+++ src/main/java/it/polito/tdp/corsi/model/Studente.java
@@ -0,0 +1,19 @@
+package it.polito.tdp.corsi.model;
+
+public class Studente {
+	
+	private Integer matricola;
+	private String cognome;
+	private String nome;
+	private String CDS;
+	public Studente(Integer matricola, String cognome, String nome, String cDS) {
+		super();
+		this.matricola = matricola;
+		this.cognome = cognome;
+		this.nome = nome;
+		CDS = cDS;
+	}
+	
+	
+
+}
